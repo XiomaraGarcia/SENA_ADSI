358b37b696cb5dc4c31903a7bf7adebef55cfb38
XiomaraGarcia <100248056+XiomaraGarcia@users.noreply.github.com>
2022-03-03 23:04:07 -0500

    Add files via upload
    Se borran algunos comentarios innecesarios.

--- IMC.py
+++ IMC.py
@@ -1,22 +1,12 @@
 # Calcula indice de masa corporal.
-
 #Variables
-#Peso 55
 weight = float(input('escriba su peso en kg :'))
-#Estatura 1,72
 height = float(input('escriba su estatura en metros:'))
 
-# 18,63456321 = 55 / (1,72)*2
 IMC = weight  / (height**2)
-
 #Imprimes por Consola.
 print('IMC:', round(IMC,1))
-#IMC: 18,6
 
-#Condicional SI y SINO
-#Condicional IF y ELSE
-
-#IMC: 18,5
 if IMC < 18.5:
     print('Underweight')
 
