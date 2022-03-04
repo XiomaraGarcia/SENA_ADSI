379dcd1a60011af96b8f06c47b0028d449907ea2
XiomaraGarcia <100248056+XiomaraGarcia@users.noreply.github.com>
2022-03-03 22:50:10 -0500

    Add files via upload
    Corrección de condicionales por errores en la lógica.

--- IMC.py
+++ IMC.py
@@ -1,17 +1,33 @@
 # Calcula indice de masa corporal.
 
-weight = float(input('escriba su peso en kg:'))
-height = float(input(' escriba su estatura en cm:'))
+#Variables
+#Peso 55
+weight = float(input('escriba su peso en kg :'))
+#Estatura 1,72
+height = float(input('escriba su estatura en metros:'))
 
+# 18,63456321 = 55 / (1,72)*2
 IMC = weight  / (height**2)
-print('IMC:', round(IMC,1))
-if IMC < 18.5:
-    print('Bajo peso')
-elif 18.5<= IMC and IMC <25:
-     print('Normal')
-elif 25<= IMC and IMC <30:
-    print('Sobre peso')
-elif IMC >=30:
-    print('Obesidad')
-print('IMC:' , IMC)
 
+#Imprimes por Consola.
+print('IMC:', round(IMC,1))
+#IMC: 18,6
+
+#Condicional SI y SINO
+#Condicional IF y ELSE
+
+#IMC: 18,5
+if IMC < 18.5:
+    print('Underweight')
+
+elif IMC >= 18.5 and IMC <25:
+     print('Normal Weight')
+     
+elif IMC >= 25 and IMC <30:
+    print('Overweight')
+
+elif IMC >=30 and IMC <35:
+    print('Obese')
+
+else:
+    print('Clinically Obese')
